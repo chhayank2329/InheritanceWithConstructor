3ed2ac2b4b94e9e8c28748ad06c95196f0728b0c
chhayank2329 <68579147+chhayank2329@users.noreply.github.com>
2020-09-12 21:26:37 +0530

    Created a Constructor of Grand Child and passing the value person_name,person_age,person_gender.

--- src/InheritanceWithConstructor/Main.java
+++ src/InheritanceWithConstructor/Main.java
@@ -9,13 +9,15 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[]args){
-        Scanner scan =new Scanner(System.in);
+        Scanner scan = new Scanner(System.in);
         System.out.println("Enter your name:");
-        String person_name=scan.next();
+        String person_name = scan.next();
         System.out.println("Enter Your Age");
-        int person_age=scan.nextInt();
+        int person_age = scan.nextInt();
         System.out.println("Enter Your Gender");
         String person_gender = scan.next();
+
+        Grand_Child gc = new Grand_Child(person_name, person_age, person_gender);
 
 
     }
